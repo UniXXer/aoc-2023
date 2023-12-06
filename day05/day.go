e61a3832d03a37c8155c3229f83ce45e2a9b54a5
UniXXer <unixxer@gmail.com>
2023-12-06 22:52:25 +0100

    day05: solve part 1 & 2

--- day05/day.go
+++ day05/day.go
@@ -1,9 +1,9 @@
 package day05
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
+	"unixxer-aoc-2023/commons"
 )
 
 func solveFirst(inputs []string) error {
@@ -23,24 +23,25 @@ func solveFirst(inputs []string) error {
 	for i := 2; i < len(inputs); i++ {
 		line := strings.TrimSpace(inputs[i])
 		switch line {
-			case "seed-to-soil map:":
-				state = line
-			case "soil-to-fertilizer map:":
-				state = line
-			case "fertilizer-to-water map:":
-				state = line
-			case "water-to-light map:":
-				state = line
-			case "light-to-temperature map:":
-				state = line
-			case "temperature-to-humidity map:":
-				state = line
-			case "humidity-to-location map:":
-				state = line
-			default: {
+		case "seed-to-soil map:":
+			state = line
+		case "soil-to-fertilizer map:":
+			state = line
+		case "fertilizer-to-water map:":
+			state = line
+		case "water-to-light map:":
+			state = line
+		case "light-to-temperature map:":
+			state = line
+		case "temperature-to-humidity map:":
+			state = line
+		case "humidity-to-location map:":
+			state = line
+		default:
+			{
 				mappings := parseSeeds(line)
-
-				switch state {
+				if len(mappings) > 0 {
+					switch state {
 					case "seed-to-soil map:":
 						seedToSoil = append(seedToSoil, mappings)
 					case "soil-to-fertilizer map:":
@@ -54,28 +55,48 @@ func solveFirst(inputs []string) error {
 					case "temperature-to-humidity map:":
 						temperatureToHumidity = append(temperatureToHumidity, mappings)
 					case "humidity-to-location map:":
-						humidityToLocation = append(humidityToLocation, mappings)					
+						humidityToLocation = append(humidityToLocation, mappings)
+					}
 				}
-			}	
+			}
 		}
 	}
 
-	fmt.Println(seeds)
-	fmt.Println(seedToSoil)
-	fmt.Println(waterToLight)
-	fmt.Println(humidityToLocation)
+	result := -1
+	for _, v := range seeds {
+		location := mapValue(mapValue(mapValue(mapValue(mapValue(mapValue(mapValue(v, seedToSoil), soilToFertilizer), fertilizerToWater), waterToLight), lightToTemperature), temperatureToHumidity), humidityToLocation)
 
-	fmt.Printf("solveFirst is not implemented yet!")
-	fmt.Println()
+		if result == -1 || result > location {
+			result = location
+		}
+	}
+
+	commons.PrintInfoFormat("The lowest location of seeds is %d", result)
+
+	// PART 2
+	result2 := -1
+	for i := 0; i < len(seeds); i = i + 2 {
+		start := seeds[i]
+		length := seeds[i+1]
+
+		for j := start; j < start+length; j++ {
+			location := mapValue(mapValue(mapValue(mapValue(mapValue(mapValue(mapValue(j, seedToSoil), soilToFertilizer), fertilizerToWater), waterToLight), lightToTemperature), temperatureToHumidity), humidityToLocation)
+
+			if result2 == -1 || result2 > location {
+				result2 = location
+			}
+		}
+
+	}
+
+	commons.PrintInfoFormat("The lowest location of seed-range is %d", result2)
 
 	return nil
-
 }
 
 func solveSecond(inputs []string) error {
 
-	fmt.Printf("solveSecond is not implemented yet!")
-	fmt.Println()
+	// not needed
 
 	return nil
 
@@ -95,6 +116,16 @@ func parseSeeds(line string) []int {
 	return seeds
 }
 
-func mapValue(value int, mapper[][]int) int {
-	
+func mapValue(value int, mapper [][]int) int {
+	for i := 0; i < len(mapper); i++ {
+		dest := mapper[i][0]
+		source := mapper[i][1]
+		length := mapper[i][2]
+
+		if value >= source && value < source+length {
+			return (value - source) + dest
+		}
+	}
+
+	return value
 }
